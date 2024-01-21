ac76b9e2227ba1afe8be428087382ba8cd25aa7e
ExtremeRus <148691113+ExtremeRus@users.noreply.github.com>
2024-01-21 14:53:01 +0500

    Add files via upload

---
+++ 34785364893.py
@@ -0,0 +1,17 @@
+#1
+# a = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# chisla = 10
+# scetchik = 0
+# scetchik2 = 1
+# while True:
+#     print(a[scetchik] + a[scetchik2])
+#     scetchik = scetchik2
+#     scetchik2 = scetchik2 + 1
+#     chisla = chisla - 1
+#     if chisla == 0:
+#         break
+
+#2
+a = input()
+b = a[::2]
+print(b)
